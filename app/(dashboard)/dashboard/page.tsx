b16d6c4aa99ef5eec4450609a5b5238f7a91c145
emdevelopa <gyimahsunday@gmail.com>
2026-01-26 11:13:07 +0100

    feat: added the deposit screen and the modals

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,47 +1,59 @@
+"use client";
 import { AccountOverview } from "@/components/dashboard/account-overview";
+import DepositMethods from "@/components/dashboard/deposit";
 import { MarketOverview } from "@/components/dashboard/market-overview";
 import { RecentTransactions } from "@/components/dashboard/recent-transactions";
 import { ArrowUpDown, Download, Upload } from "lucide-react";
+import { useState } from "react";
+
 
 export default function DashboardPage() {
-    return (
-        <div className="flex flex-col gap-5 md:gap-10">
-            <AccountOverview />
+  const [openDeposit, setOpenDeposit] = useState(false);
+  const toggleDeposit = () => {
+    setOpenDeposit(!openDeposit);
+  };
 
-            <div className="md:px-4 space-y-4">
-                <div className="grid grid-cols-3 gap-4 px-6 pb-6 md:p-0">
-                    <div className="flex flex-col items-center justify-center bg-card rounded-xl md:rounded-sm py-6 md:py-10 gap-2 border-[0.43px] border-[#79797966]">
-                        <Download />
-                        <p className="text-sm md:text-base font-medium">
-                            Deposit
-                        </p>
-                    </div>
-                    <div className="flex flex-col items-center justify-center bg-card rounded-xl md:rounded-sm py-6 md:py-10 gap-2 border-[0.43px] border-[#79797966]">
-                        <Upload />
-                        <p className="text-sm md:text-base font-medium">
-                            Withdraw
-                        </p>
-                    </div>
-                    <div className="flex flex-col items-center justify-center bg-card rounded-xl md:rounded-sm py-6 md:py-10 gap-2 border-[0.43px] border-[#79797966]">
-                        <ArrowUpDown />
-                        <p className="text-sm md:text-base font-medium">
-                            Convert
-                        </p>
-                    </div>
-                </div>
+  return (
+    <div className="flex flex-col gap-5 md:gap-10">
+      <AccountOverview openDeposit={openDeposit} />
 
-
-                <div className="space-y-4 px-3 md:px-0">
-                    <div className="flex items-center justify-between">
-                        <h3 className="text-sm md:text-lg font-semibold">Exchange Rates</h3>
-                        <p className="text-xs md:text-sm text-muted-foreground">Live updates</p>
-                    </div>
-                    <MarketOverview />
-                </div>
-
-                <RecentTransactions />
+      {openDeposit ? (
+        <DepositMethods toggleDeposit={toggleDeposit} />
+      ) : (
+        <div className="md:px-4 space-y-4">
+          <div className="grid grid-cols-3 gap-4 px-6 pb-6 md:p-0">
+            <div
+              className="flex flex-col items-center justify-center bg-card rounded-xl md:rounded-sm py-6 md:py-10 gap-2 border-[0.43px] border-[#79797966]"
+              onClick={toggleDeposit}
+            >
+              <Download />
+              <p className="text-sm md:text-base font-medium">Deposit</p>
             </div>
+            <div className="flex flex-col items-center justify-center bg-card rounded-xl md:rounded-sm py-6 md:py-10 gap-2 border-[0.43px] border-[#79797966]">
+              <Upload />
+              <p className="text-sm md:text-base font-medium">Withdraw</p>
+            </div>
+            <div className="flex flex-col items-center justify-center bg-card rounded-xl md:rounded-sm py-6 md:py-10 gap-2 border-[0.43px] border-[#79797966]">
+              <ArrowUpDown />
+              <p className="text-sm md:text-base font-medium">Convert</p>
+            </div>
+          </div>
 
+          <div className="space-y-4 px-3 md:px-0">
+            <div className="flex items-center justify-between">
+              <h3 className="text-sm md:text-lg font-semibold">
+                Exchange Rates
+              </h3>
+              <p className="text-xs md:text-sm text-muted-foreground">
+                Live updates
+              </p>
+            </div>
+            <MarketOverview />
+          </div>
+
+          <RecentTransactions />
         </div>
-    );
+      )}
+    </div>
+  );
 }
